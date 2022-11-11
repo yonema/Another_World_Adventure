393fb85ae17cfc85f28d8a7f46158046526566e7
AzumaHiroki <poohfamily1017@gmail.com>
2022-11-11 10:31:11 +0900

    プレイヤーのMPを実装。_DEBUGモードとして、L1+Aでサンプルスプライト生成（プレイヤーのMP量が大きさに反映される）。L1+Bで弱スキル分、L1+Xで強スキル分MPを消費する。さらに、MPが自動回復するようになった。

--- Game/Game/Player/PlayerStatus.h
+++ Game/Game/Player/PlayerStatus.h
@@ -10,15 +10,25 @@ namespace nsAWA {
 		public:
 			void Init();
 
-			void DamageHP(int damage);
+			void HealHP(int value);
+			void DamageHP(int value);
+
+			void HealMP(int value);
+			void DamageMP(int value);
 		public:
 			int GetHP()const {
 
 				return m_HP;
 			}
+			int GetMP()const {
+
+				return m_MP;
+			}
 		private:
 			int m_HP = 0;		//HP
 			int m_maxHP = 0;	//ç≈ëÂHP
+			int m_MP = 0;		//MP
+			int m_maxMP = 0;	//ç≈ëÂMP
 		};
 	}
 }
